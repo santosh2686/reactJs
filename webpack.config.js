678711142ad6cd2f7fa7eade884bf403583a4480
santosh <santosh2686@gmail.com>
2016-06-16 21:02:18 -0700

    added git ignore and add script to package json

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports={
 	entry:'./main.js',
-	output:{path:__dirname, filename:'bundle.js'},
+	output:{path:__dirname+'/dist', filename:'bundle.js'},
 	module:{
 		loaders:[{
 			test:/.jsx?$/,
